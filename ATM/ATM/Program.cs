9cd0fac53a6ac8c73029ea320df69b67ae764aba
Sudan Shakya <sudanshakya15@gmail.com>
2023-04-20 21:46:45 +0545

    deposit, withdraw and balance function added

--- ATM/ATM/Program.cs
+++ ATM/ATM/Program.cs
@@ -69,6 +69,43 @@ public class cardHolder
 
     public static void Main(String[] args)
     {
+        void printOptions()
+        {
+            Console.WriteLine("Please choose from one of the following options...");
+            Console.WriteLine("1. Deposit");
+            Console.WriteLine("2. Withdraw");
+            Console.WriteLine("3. Show Balance");
+            Console.WriteLine("4. Exit")
+        }
 
+        void deposit(cardHolder currentUser)
+        {
+            Console.WriteLine("How much money would you like to deposit? ");
+            double deposit = Double.Parse(Console.ReadLine());
+            currentUser.setBalance(deposit);
+            Console.WriteLine("Thank you. Your new balance is: " + currentUser.getBalance);
+        }
+
+        void withdraw(cardHolder currentUser)
+        {
+            Console.WriteLine("How much money would you like to withdraw? ");
+            double withdrawal = Double.Parse(Console.ReadLine());
+
+            //To check if the user has enough money
+            if(currentUser.getBalance() > withdrawal) 
+            {
+                Console.WriteLine("Insufficient balance...");
+            }
+            else
+            {
+                currentUser.setBalance(currentUser.getBalance() - withdrawal);
+                Console.WriteLine("Thank you. Have a great day.");
+            }
+        }
+
+        void balance(cardHolder currentUser)
+        {
+            Console.WriteLine("Current balance: " + currentUser.getBalance());
+        }
     }
 }
